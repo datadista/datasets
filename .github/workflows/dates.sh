93bf7231ba47d6252fa4df365fb1d8c8f9b4e22a
JJ Merelo <jjmerelo@gmail.com>
2020-03-26 09:53:16 +0100

    Saca pa fuera

---
+++ .github/workflows/dates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat .github/workflows/header.md > fechas.md
+git config --global user.email "jjmerelo@gmail.com"
+git config --global user.name "FechaActionBot"
+test=$(git log --date=short --format="%ad")
+echo "Test $test"
+git ls-tree -r --name-only HEAD COVID\ 19/*.csv | while read filename; do
+    date=$(git log -1 --date=short --format="%ad" -- $filename)
+    echo "Fecha $date"
+    echo "| $date  | $filename |" >> fechas.md
+done
+git add fechas.md
+git commit -m "Fichero de fechas generado"
