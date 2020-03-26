24b57cf43fa59cb4c43a23194e692bbfb0151d62
FechaActionBot <jjmerelo@gmail.com>
2020-03-26 11:05:34 +0100

    Cambia nombre de fichero

--- .github/workflows/dates.sh
+++ .github/workflows/dates.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cat .github/workflows/header.md > fechas.md
+cat .github/workflows/header.md > COVID\ 19/fechas.md
 git config --global user.email "jjmerelo@gmail.com"
 git config --global user.name "FechaActionBot"
-echo "Test $test"
 git ls-tree -r --name-only HEAD COVID\ 19/*.csv | while read filename; do
     date=$(git log -1 --date=short --format="%cd" -- "$filename")
-    echo "| $date  | $filename |" >> fechas.md
+    echo "| $date  | $filename |" >>  COVID\ 19/fechas.md
 done
 git add fechas.md
 git commit -m "Fichero de fechas generado"
