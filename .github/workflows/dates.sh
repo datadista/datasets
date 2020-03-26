5eb4b200b13bfd8e45e29cb66790f87aebb98e18
JJ Merelo <jjmerelo@gmail.com>
2020-03-26 10:00:51 +0100

    Con comillas

--- .github/workflows/dates.sh
+++ .github/workflows/dates.sh
@@ -3,10 +3,11 @@
 cat .github/workflows/header.md > fechas.md
 git config --global user.email "jjmerelo@gmail.com"
 git config --global user.name "FechaActionBot"
-test=$(git log --date=short --format="%ad" -- COVID\ 19/nacional_covid19.csv)
+filename="COVID\ 19/nacional_covid19.csv"
+test=$(git log --date=short --format="%ad" -- $filename)
 echo "Test $test"
 git ls-tree -r --name-only HEAD COVID\ 19/*.csv | while read filename; do
-    date=$(git log -1 --date=short --format="%ad" -- $filename)
+    date=$(git log -1 --date=short --format="%ad" -- "$filename")
     echo "Fecha $date"
     echo "| $date  | $filename |" >> fechas.md
 done
