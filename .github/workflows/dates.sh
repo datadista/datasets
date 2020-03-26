c0d383c4eebe62737a141f76104392e14e9f5982
FechaActionBot <jjmerelo@gmail.com>
2020-03-26 10:05:09 +0100

    Modificación final para fecha de commit

--- .github/workflows/dates.sh
+++ .github/workflows/dates.sh
@@ -3,12 +3,9 @@
 cat .github/workflows/header.md > fechas.md
 git config --global user.email "jjmerelo@gmail.com"
 git config --global user.name "FechaActionBot"
-filename="COVID\ 19/nacional_covid19.csv"
-test=$(git log --date=short --format="%ad" -- $filename)
 echo "Test $test"
 git ls-tree -r --name-only HEAD COVID\ 19/*.csv | while read filename; do
-    date=$(git log -1 --date=short --format="%ad" -- "$filename")
-    echo "Fecha $date"
+    date=$(git log -1 --date=short --format="%cd" -- "$filename")
     echo "| $date  | $filename |" >> fechas.md
 done
 git add fechas.md
